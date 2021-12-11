a96d143ea001cf51267b5b6713db4d67640dc4ec
Vanaf1979 <vanaf1979@gmail.com>
2021-12-11 13:57:11 +0100

    Updated to multifile addon

--- __init__.py
+++ __init__.py
@@ -1,136 +1,22 @@
-# ----------------------------------------------------------------------
-# Align Origin
-# ----------------------------------------------------------------------
-# Package             Align Origin
-# Version             0.2.0
-# Description         Align origin to top, center or bottom of mesh
-# Author              Stephan Nijman <vanaf1979@gmail.com>
-# Author URI          https://since1979.dev
-# Copyright           2021 Stephan Nijman
-# Plugin repo         https://github.com/vanaf1979/align-origin
-# Requires at least   3.0.0
-# License:            GPL v2 or later
-# License URI         http://www.gnu.org/licenses/gpl-2.0.txt
-# ----------------------------------------------------------------------
-
-
-import bpy
-import mathutils
-
-
-# ----------------------------------------------------------------------
-# REGISTER THIS ADDON
-# ----------------------------------------------------------------------
 bl_info = {
-    "name" : "Align origin",
+    "name" : "Align Origin",
     "author" : "Stephan Nijman",
-    "description" : "Align origin to top, center or bottom of mesh.",
+    "description" : "Align origin to mesh",
     "blender" : (3, 0, 0),
-    "version" : (0, 2, 0),
+    "version" : (0, 2, 1),
     "location" : "3DView",
     "warning" : "",
-    "category" : "Utils"
+    "category" : "Generic"
 }
 
+#from . import addon_updater_ops
+from . import auto_load
 
+auto_load.init()
 
-# ----------------------------------------------------------------------
-# ALIGN ORIGIN TO TOP, CENTER OR BOTTOM OF MESH
-# ----------------------------------------------------------------------
-class ST_OT_Origin_To_Base(bpy.types.Operator):
-    """Align origin to mesh"""
-    bl_label = "Align Origin to mesh"
-    bl_idname = 'object.align_origin_to_mesh'
-
-    location: bpy.props.StringProperty(default = 'bottom')
-    
-    
-    @classmethod
-    def poll(cls, context):
-        return context.object is not None and context.object.type in {'MESH', 'CURVE'}
-    
-    
-    def execute(self, context):
-
-        active = context.object
-
-        if context.object is None or context.object.type not in {'MESH', 'CURVE'}:
-            self.report({'ERROR'}, "No object selected")
-            return {'FINISHED'}
-
-        old_curs_loc = mathutils.Vector(bpy.context.scene.cursor.location)
-
-        bpy.ops.object.origin_set(type='ORIGIN_GEOMETRY', center='MEDIAN')
-        
-        
-        matrix_world = active.matrix_world
-        vertex_coordinates = [ matrix_world @ v.co for v in active.data.vertices ]
-        
-        if self.location == 'bottom':
-            new_z = min( [ co.z for co in vertex_coordinates ] ) 
-            
-        elif self.location == 'top':
-            new_z = max( [ co.z for co in vertex_coordinates ] ) 
-            
-        else:
-            min_z = min( [ co.z for co in vertex_coordinates ] ) 
-            max_z = max( [ co.z for co in vertex_coordinates ] )
-            new_z = max_z + (min_z-max_z) / 2
-            
-
-        context.scene.cursor.location = mathutils.Vector((active.location.x,active.location.y,new_z))
-
-        bpy.ops.object.origin_set(type='ORIGIN_CURSOR', center='MEDIAN')
-
-        context.scene.cursor.location = old_curs_loc
-
-        return {'FINISHED'}
-
-
-
-# ----------------------------------------------------------------------
-# CREATE THE 3DVIEW OBJECT CONTEXT SUBMENU
-# ----------------------------------------------------------------------
-class ST_MT_Align_Origin(bpy.types.Menu):
-    bl_label = "Align Origin"
-    bl_idname = "OBJECT_MT_align_origin"
-
-    def draw(self, context):
-        curs_bt = self.layout.operator('object.align_origin_to_mesh', text='Origin to top')
-        curs_bt.location = 'top'
-        curs_bt = self.layout.operator('object.align_origin_to_mesh', text='Origin to center')
-        curs_bt.location = 'center'
-        curs_bt = self.layout.operator('object.align_origin_to_mesh', text='Origin to bottom')
-        curs_bt.location = 'bottom'
-
-
-
-# ----------------------------------------------------------------------
-# ADD SUBMENU TO 3DVIEW OBJECT CONTEXT MENU
-# ----------------------------------------------------------------------
-def align_origin_object_context_menu(self, context):
-    
-    if context.object is not None and context.object.type in {'MESH', 'CURVE'}:
-        self.layout.separator()
-        self.layout.menu(ST_MT_Align_Origin.bl_idname)
-    
-
-
-# ----------------------------------------------------------------------
-# REGISTER SUBMENU
-# ----------------------------------------------------------------------
 def register():
-    bpy.utils.register_class(ST_OT_Origin_To_Base)
-    bpy.utils.register_class(ST_MT_Align_Origin)
-    bpy.types.VIEW3D_MT_object_context_menu.prepend(align_origin_object_context_menu)
-    bpy.types.VIEW3D_MT_object.prepend(align_origin_object_context_menu)
+    auto_load.register()
+    #addon_updater_ops.register(bl_info)
 
 def unregister():
-    bpy.utils.unregister_class(ST_OT_Origin_To_Base)
-    bpy.utils.unregister_class(ST_MT_Align_Origin)
-    bpy.types.VIEW3D_MT_object_context_menu.remove(align_origin_object_context_menu)
-    bpy.types.VIEW3D_MT_object.remove(align_origin_object_context_menu)
-    
-    
-if __name__ == "__main__":
-    register()
+    auto_load.unregister()
---
+++ addon_prefs_panel.py
@@ -0,0 +1,49 @@
+import bpy
+
+class AlignOriginsPreferences(bpy.types.AddonPreferences):
+	"""Align origins addon preferences"""
+	bl_idname = __package__
+
+	# Addon updater preferences.
+
+	auto_check_update = bpy.props.BoolProperty(
+		name="Auto-check for Update",
+		description="If enabled, auto-check for updates using an interval",
+		default=False)
+
+	updater_interval_months = bpy.props.IntProperty(
+		name='Months',
+		description="Number of months between checking for updates",
+		default=0,
+		min=0)
+
+	updater_interval_days = bpy.props.IntProperty(
+		name='Days',
+		description="Number of days between checking for updates",
+		default=7,
+		min=0,
+		max=31)
+
+	updater_interval_hours = bpy.props.IntProperty(
+		name='Hours',
+		description="Number of hours between checking for updates",
+		default=0,
+		min=0,
+		max=23)
+
+	updater_interval_minutes = bpy.props.IntProperty(
+		name='Minutes',
+		description="Number of minutes between checking for updates",
+		default=0,
+		min=0,
+		max=59)
+
+	def draw(self, context):
+		layout = self.layout
+
+		# Works best if a column, or even just self.layout.
+		mainrow = layout.row()
+		col = mainrow.column()
+
+		# Updater draw function, could also pass in col as third arg.
+		addon_updater_ops.update_settings_ui(self, context)
